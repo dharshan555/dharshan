44e694237569635fd88fc38d3c92294643140336
dharshan555 <dharshanjayaraman78@gmail.com>
2025-05-25 21:01:53 +0530

    update greaternum.py

--- python basics/greaternum.py
+++ python basics/greaternum.py
@@ -1,11 +1,11 @@
-a=int(input("enter a : "))
-b=int(input("enter b : "))
-c=int(input("enter c : "))
-if a>b:
-    print(a)
-elif b>c:
-    print(b)
-elif c>a:
-    print(c)
+a=int(input("num 1 "))
+b=int(input("num 2 "))
+c=int(input("num 3 "))
+if a>b and a>c:
+    print(a,"is greater")
+elif b>c and b>a:
+    print(b,"is greater")
+elif c>a and c>b:
+    print(c,"is greater")
 else:
-    print("all num are equal")
+    print("invalid numbers")
